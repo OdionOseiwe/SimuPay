1a71f93b95f2da8d7906cc5f4b19a8a37d3c644f
OdionOseiwe <oseiweifebhor@gmail.com>
2025-11-06 08:36:34 +0100

    fixed mails

--- backend/Mail/mailConfig.js
+++ backend/Mail/mailConfig.js
@@ -2,9 +2,15 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for port 465
   auth: {
     user: "oseiweifebhor@gmail.com",
     pass: process.env.APP_PASSWORD 
+  },
+  tls: {
+    rejectUnauthorized: false, // helps if behind strict firewalls
   },
 });
 
